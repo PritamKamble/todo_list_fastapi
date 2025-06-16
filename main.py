de193e8eb14ba2cbdbc509f166b95767fca8b3a3
Pritam Kamble <7276279026.pk@gmail.com>
2025-06-16 12:55:11 +0530

    new api added

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class LoginBody(BaseModel):
+    email: str
+    password: str
 
 app = FastAPI()
 
 @app.get("/hello")
 def read_root():
     return { "msg": "Hello, World!" }
+
+@app.get('/add/{no1}/{no2}')
+def add(no1: int, no2: int):
+    return { 'total': no1 + no2 }
+
+@app.post('/login')
+def login(loginBody: LoginBody):
+    print(loginBody.email)
+    return { 'msg': 'Login successful' }
